093a659f70304baf8b36e78a8bd7382f097122ea
Me <nicko2duval@gmail.com>
2018-03-19 09:33:07 -0600

    Add files via upload

--- Day.java
+++ Day.java
@@ -1,36 +1,43 @@
 import java.util.*;
 import java.awt.*;
-import java.swing.*;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Day{
+	/*
 	private Scanner in = new Scanner(System.in); //clock in time
 	private Scanner out = new Scanner(System.in); //clock out time
 	private Scanner wage = new Scanner(System.in); //hourly wage
-	private Scanner income = new Scanner(System.in); //yearly income
+	private Scanner income = new Scanner(System.in); //yearly income  
+	*/
 	private JFrame day = new JFrame();
-	private JLabel header = new JLabel("Day", JLabel.CENTER); //label window as day
+	private JPanel controlPanel = new JPanel();
+	private JLabel w = new JLabel("calculate with hourly wage", JLabel);
+	private JLabel i = new JLabel("calculate with yearly income", JLabel);
 	private JButton wage = new JButton("wage", JButton.CENTER); //button to choose whether or not to use hourly wage	
 	private JButton income = new JButton("income", JButton.CENTER); //or yearly income
 	private JTextField hourlyWage = new JTextField(); //textbox 
 	private JTextField yearlyIncome = new JTextField();
 
-	public static void main(String[] args){
-		
+	public void setup(){
+		day = new JFrame("Day");
 		day.setVisible(true);
 		day.setSize(500,500);
+		day.add(controlPanel);
+		day.add.setVisible(true);
 
+		controlPanel = new JPanel();
+		controlPanel.setLayout(new FlowLayout());
+}
+	public void window(){
 		controlPanel.add(wage);
+		controlPanel.add(userText);
 		controlPanel.add(income);
-
-		println("what was your clock in time in military time?");
-		int in = in.nextInt();
-
-		println("what was your clock out time in military time?");
-		int out = out.nextInt();
+		controlPanel.add(userText);
 
 	}
 
-	public void Button(){
+	public void button(){
 		wage.addActionListerner(new ActionListener(){
 			public void actionPreformed(ActionEvent a){
 				add.hourlyWage();
