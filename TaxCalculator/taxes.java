db532edda1f515000c616e70debeb5f1b6d2aad0
Nirvik Sharma <sharmanirvik1@gmail.com>
2018-03-21 08:48:13 -0600

    Add files via upload

---
+++ TaxCalculator/taxes.java
@@ -0,0 +1,18 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class taxes {
+	public static void main(String[] args){
+		JFrame mf = new JFrame();
+		JButton b1 = new JButton();
+		mf.setSize(new Dimension(500,650));
+		mf.setLocationRelativeTo(null);
+		mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mf.setTitle("Tax APP");
+		mf.setVisible(true);
+		b1.setSize(new Dimension(50,50));
+	}
+
+}
